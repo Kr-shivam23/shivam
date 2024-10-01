b20e7276a26d293ad84a884ee22fb6205482911c
Administrator <Administrator@DESKTOP-4MKSC1R>
2024-10-01 12:48:29 +0530

    Build B03

---
+++ selenium/src/framework/Demo1.java
@@ -0,0 +1,20 @@
+package framework;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+
+	@Test
+	public void test1()
+	{
+		Reporter.log("Kumar",true);
+	}
+	@Test
+	public void test2()
+	{
+		Reporter.log("Shivam",true);
+	}
+	
+}
